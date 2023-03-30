c84e8332c056576c93df28a71111abe5c5a9d421
Espen Zachrisen <espen.zachrisen@alticeusa.com>
2023-03-30 16:56:39 +0000

    updated bootstrap with mosh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,17 @@
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # install mosh with true color support
-sudo add-apt-repository ppa:keithw/mosh-dev
-sudo apt update
-sudo apt install -y mosh
+sudo mv /usr/local/bin/protoc /usr/local/bin/protoc-new	
+sudo apt-get install debhelper autotools-dev protobuf-compiler libprotobuf-dev dh-autoreconf pkg-config libutempter-dev zlib1g-dev libncurses5-dev libssl-dev bash-completion locales -y
+cd /tmp
+git clone https://github.com/mobile-shell/mosh
+cd mosh
+./autogen.sh
+./configure
+make
+sudo make install
+sudo mv /usr/local/bin/protoc-new /usr/local/bin/protoc
+
+echo "alias 'd'='cd ~/.config/dev && nix develop'" >> ~/.bashrc 
+echo "alias 'd2'='cd ~/.config/dev2 && nix develop'" >> ~/.bashrc 
 
